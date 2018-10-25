5b3d6d275bc22013183398d9b3f85e97ea009204
igorench <iturovskij5@gmail.com>
2018-10-25 11:53:32 +0300

    fix_renderHTML

--- main.js
+++ main.js
@@ -12,11 +12,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
     btn.addEventListener("click", function () {
         ourRequest.open('GET', `https://learnwebcode.github.io/json-example/animals-${pageCounter}.json`);         // GET - при получении данных с сервера, POST - при отправке данных на сервер
-        ourRequest.onload = function () {                // событие - когда наши данные загрузились
-            let ourData = JSON.parse(ourRequest.responseText); // сохраняем полученные данные в переменную data  // JSON.parse() - говорим js о том что мы считываем JSON файл
-            console.log(ourData[0]);
-            renderHTML(ourData);
+        ourRequest.onload = function () {       // событие - когда наши данные загрузились
+            if (ourRequest.status >= 200 && ourRequest.status < 400) {
+                let ourData = JSON.parse(ourRequest.responseText); // сохраняем полученные данные в переменную data  // JSON.parse() - говорим js о том что мы считываем JSON файл
+                renderHTML(ourData);
+            } else {
+                throw("We connected to the server, but it returned an error.");
+            }
         };
+
+        ourRequest.onerror = function() {
+            throw("Connection error!");
+        };
+
         ourRequest.send(); // отправляем запрос
 
         pageCounter++;
@@ -29,15 +37,31 @@ document.addEventListener("DOMContentLoaded", function () {
         let htmlString = "";
 
         for (let i = 0; i < data.length; i++) {
-            htmlString += "<p>" + data[i].name + " is a " + data[i].species + " that likes to eat";
+            htmlString += "<p>" + data[i].name + " is a " + data[i].species + " that likes to eat ";
 
-            for (let j = 0; j < data[i].foods.likes.length; i++) {
-                htmlString += data[i].foods.likes[j];
+            for (let ii = 0; ii < data[i].foods.likes.length; ii++) {
+                if (ii === 0) {
+                    htmlString += data[i].foods.likes[ii];
+                } else {
+                    htmlString += " and " + data[i].foods.likes[ii];
+                }
             }
-            htmlString += ".</p>";
+
+            htmlString += ' and dislikes ';
+
+            for (let ii = 0; ii < data[i].foods.dislikes.length; ii++) {
+                if (ii === 0) {
+                    htmlString += data[i].foods.dislikes[ii];
+                } else {
+                    htmlString += " and " + data[i].foods.dislikes[ii];
+                }
+            }
+
+            htmlString += '.</p>';
+
         }
 
-        animalContainer.insertAdjacentHTML('beforeend', htmlString); // разбирает текст как HTML или XML и вставляет полученные узлы(nodes) в DOM дерево в указанную позицию
+        animalContainer.insertAdjacentHTML('beforeend', htmlString);
     }
 
 
