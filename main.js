83c065d0bb5bf7de8077377b025666b342993358
igorench <iturovskij5@gmail.com>
2018-10-25 11:17:14 +0300

    like_food

--- main.js
+++ main.js
@@ -7,20 +7,21 @@ document.addEventListener("DOMContentLoaded", function () {
     let ourRequest = new XMLHttpRequest();    // Создаем новый экземпляр объекта запроса
     let btn = document.getElementById("btn");
     let animalContainer = document.getElementById("animal-info");
-    let counterUrl = 1;
+    let pageCounter = 1;
+
 
     btn.addEventListener("click", function () {
-            ourRequest.open('GET', `https://learnwebcode.github.io/json-example/animals-${counter}.json`);         // GET - при получении данных с сервера, POST - при отправке данных на сервер
-            ourRequest.onload = function () {                // событие - когда наши данные загрузились
-                let ourData = JSON.parse(ourRequest.responseText); // сохраняем полученные данные в переменную data  // JSON.parse() - говорим js о том что мы считываем JSON файл
-                renderHTML(ourData);
-            };
-            ourRequest.send(); // отправляем запрос
+        ourRequest.open('GET', `https://learnwebcode.github.io/json-example/animals-${pageCounter}.json`);         // GET - при получении данных с сервера, POST - при отправке данных на сервер
+        ourRequest.onload = function () {                // событие - когда наши данные загрузились
+            let ourData = JSON.parse(ourRequest.responseText); // сохраняем полученные данные в переменную data  // JSON.parse() - говорим js о том что мы считываем JSON файл
+            console.log(ourData[0]);
+            renderHTML(ourData);
+        };
+        ourRequest.send(); // отправляем запрос
 
-        if (counterUrl >= 3) {
-            counterUrl = 1;
-        } else {
-            counterUrl++;
+        pageCounter++;
+        if (pageCounter > 3) {
+            btn.classList.add("btn__hide");
         }
     });
 
@@ -28,11 +29,17 @@ document.addEventListener("DOMContentLoaded", function () {
         let htmlString = "";
 
         for (let i = 0; i < data.length; i++) {
-            htmlString += `<p> ${data[i].name} is a ${data[i].species}.</p>`
+            htmlString += "<p>" + data[i].name + " is a " + data[i].species + " that likes to eat";
+
+            for (let j = 0; j < data[i].foods.likes.length; i++) {
+                htmlString += data[i].foods.likes[j];
+            }
+            htmlString += ".</p>";
         }
 
         animalContainer.insertAdjacentHTML('beforeend', htmlString); // разбирает текст как HTML или XML и вставляет полученные узлы(nodes) в DOM дерево в указанную позицию
     }
+
 
 
 }, false);
